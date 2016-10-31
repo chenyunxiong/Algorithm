c0708cce02fbcf4c9e5c4d2686825ce6d46f5d15
cyx <1252315868@qq.com>
2016-10-31 16:43:05 +0800

    02----4

---
+++ Assets/Scripts/Algorithm4.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Algorithm : MonoBehaviour {
+
+	void Start()
+	{
+		Sum();
+	}
+	
+    private int Sum( int num1, int num2, int num3)
+    {
+        int sum = num1 + num2 + num3;
+        return sum;
+    }
+}
