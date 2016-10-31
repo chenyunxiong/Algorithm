8c8b3cc19748386b221225cea8bbabb6f979960e
cyx <1252315868@qq.com>
2016-10-31 14:26:23 +0800

    sum 成为全局

--- Assets/Scripts/Algorithm.cs
+++ Assets/Scripts/Algorithm.cs
@@ -5,10 +5,11 @@ public class Algorithm : MonoBehaviour {
 
     private int number1;
     private int number2;
+	private int sum;
 
     private int Sum( int num1, int num2)
     {
-        int sum = num2 + num1;
+        sum = num2 + num1;
         return sum;
     }
 }
