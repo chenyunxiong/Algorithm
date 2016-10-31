e3f0f524ec0b56046ed1ee78202dc4dae48121c5
chenyunxiong <125231568@qq.com>
2016-10-31 13:50:41 +0800

    count sum

--- Assets/Scripts/Algorithm.cs
+++ Assets/Scripts/Algorithm.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class Algorithm : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private int number1;
+    private int number2;
+
+    private int Sum( int num1, int num2)
+    {
+        int sum = num2 + num1;
+        return sum;
+    }
 }
