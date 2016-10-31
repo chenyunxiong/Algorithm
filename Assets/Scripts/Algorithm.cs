592e82c4b8e59f6b9a064f4b44c261c33b133a4b
cyx <1252315868@qq.com>
2016-10-31 17:45:14 +0800

    01   11111111111111111

--- Assets/Scripts/Algorithm.cs
+++ Assets/Scripts/Algorithm.cs
@@ -82,8 +82,12 @@ public class Algorithm : MonoBehaviour {
     }
 	 private int Sum( int num1, int num2, int num3)
     {
+        int sum = num2 + num1 + num1;
         sum += chenyunxiong * 2;
 		sum = chenyunxiong;		
+		int xiong;
+		int chen;
+		int yun;
         return sum;
     }
 	private int Sum( int num1, int num2, int num3)
